e352cf8545c297ebbebbd69844f1ce09f93eb84a
thomasreilly1011 <thomas.edreilly@gmail.com>
2020-02-14 19:30:53 +0000

    Implemented methods for LinkedDequeue.

--- src/projectCode20280/LinkedDeque.java
+++ src/projectCode20280/LinkedDeque.java
@@ -2,57 +2,56 @@ package projectCode20280;
 
 public class LinkedDeque<E> implements Deque<E> {
 
+	DoublyLinkedList<E> dll = new DoublyLinkedList<>();
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		return dll.size();
 	}
 
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
+		return dll.isEmpty();
 	}
 
 	@Override
 	public E first() {
-		// TODO Auto-generated method stub
-		return null;
+		return dll.get(0);
 	}
 
 	@Override
 	public E last() {
-		// TODO Auto-generated method stub
-		return null;
+		return dll.get(size()-1);
 	}
 
 	@Override
 	public void addFirst(E e) {
-		// TODO Auto-generated method stub
+		dll.addFirst(e);
 		
 	}
 
 	@Override
 	public void addLast(E e) {
-		// TODO Auto-generated method stub
+		dll.addLast(e);
 		
 	}
 
 	@Override
 	public E removeFirst() {
-		// TODO Auto-generated method stub
-		return null;
+		return dll.removeFirst();
 	}
 
 	@Override
 	public E removeLast() {
-		// TODO Auto-generated method stub
-		return null;
+		return dll.removeLast();
 	}
 
+	@Override
+	public String toString() {
+		return dll.toString();
+	}
 }
