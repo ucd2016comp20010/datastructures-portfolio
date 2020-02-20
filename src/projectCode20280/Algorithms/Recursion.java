7d04abc950beb7a5b9a06b7d660e0267b9978d1a
thomasreilly1011 <thomas.edreilly@gmail.com>
2020-02-20 19:18:46 +0000

    Added Recursion Class to hold any recursion related algorithms. Implemented recursive reverseArray method.

---
+++ src/projectCode20280/Algorithms/Recursion.java
@@ -0,0 +1,25 @@
+package projectCode20280.Algorithms;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Recursion {
+
+    public static int[] reverseArray(int[] a, int i, int j) {
+        if (i < j) {
+            int temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+            a = reverseArray(a, i + 1, j - 1);
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        //Testing reverseArray method
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8};
+        System.out.println("Original array: " + Arrays.toString(arr));
+        System.out.println("Reversed array: " + Arrays.toString(reverseArray(arr, 0, 7)));
+    }
+
+}
