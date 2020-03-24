99dbf033a55412dd18318ab0c986191031792c1d
tommy1011 <thomas.reilly@ucdconnect.ie>
2020-03-24 17:04:07 +0000

    Added PQSort

---
+++ src/projectCode20280/Algorithms/PQSort.java
@@ -0,0 +1,18 @@
+package projectCode20280.Algorithms;
+
+public class PQSort {
+
+    public void PQSort(E[] a) {
+        //First Phase
+        //Move all teh elements from Array -> PQ
+
+
+    }
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
